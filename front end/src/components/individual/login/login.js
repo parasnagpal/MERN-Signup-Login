191a08dcad46ef77758cd6420552602b1c248089
Paras Nagpal <paras.nag@gmail.com>
2020-06-06 04:33:26 +0530

    Capturing login input

--- front end/src/components/individual/login/login.js
+++ front end/src/components/individual/login/login.js
@@ -2,6 +2,28 @@ import React from 'react'
 import {Input,Form,FormGroup,Button} from 'reactstrap'
 
 class LoginIndividual extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={
+            email:"",
+            password:""
+        };
+        this.emailchange=this.emailchange.bind(this);
+        this.passwordchange=this.passwordchange.bind(this);
+    }
+
+    emailchange(event){
+        this.setState({
+            email:event.target.value
+        })
+    }
+
+    passwordchange(event){
+        this.setState({
+            password:event.target.value
+        })
+    }
+
     render(){
         return(
             <div className="d-flex flex-column height_100">
@@ -9,13 +31,13 @@ class LoginIndividual extends React.Component{
                 <div className="d-flex flex-column justify-content-center flex-grow-1">
                     <Form>
                         <FormGroup>
-                            <Input placeholder="Email" className="bg-light"/>
+                            <Input placeholder="Email" className="bg-light"  type='email' value={this.state.email} onChange={this.emailchange}/>
                         </FormGroup>
                         <FormGroup>    
-                            <Input placeholder="Password" className="bg-light"/>
+                            <Input placeholder="Password" className="bg-light" type='password' value={this.state.password} onChange={this.passwordchange}/>
                         </FormGroup>
                         <FormGroup>
-                        <Button color="danger" className="rounded m-2">Sign In</Button>
+                        <Button color="danger" className="rounded m-2" onClick={()=>{this.props.login(this.state)}}>Sign In</Button>
                         </FormGroup>
                     </Form>
                 </div>
