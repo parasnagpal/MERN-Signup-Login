d204d9daac0ad29dee7229730054a48392b7875a
Paras Nagpal <paras.nag@gmail.com>
2020-06-06 04:30:44 +0530

    mongo and API

--- back-end/index.js
+++ back-end/index.js
@@ -1,8 +1,33 @@
 const express=require('express');
 const app=express();
+const MongoClient=require('mongodb').MongoClient;
+
+const url='mongodb://localhost:27017';
+const dbName='mern';
+
+MongoClient.connect(url,(err,client)=>{
+    console.log("Connected successfully to server");
+    const db=client.db(dbName);
+    client.close();
+})
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended:true
+}))
 
 app.get('/',function(req,res){
-    res.send('Hello world')
+    res.send('Hello world');
+})
+
+app.post('/login',function(req,res){
+    console.log(req.body);
+    res.send('received login details');
+})
+
+app.post('/signup',function(req,res){
+    console.log(req.body);
+    res.send('received signup details');
 })
 
 app.listen(8000,()=>{
