d2a8945f2f9a0396f414317bd06dd1fbf595f600
Paras Nagpal <paras.nag@gmail.com>
2020-06-06 04:34:31 +0530

    API request to express server

--- front end/src/components/individual/index.js
+++ front end/src/components/individual/index.js
@@ -2,16 +2,46 @@ import React from 'react'
 import { Row ,Container,Col} from 'reactstrap'
 import LoginIndividual from './login/login'
 import SignUpIndividual from './signup/signup'
+import axios from 'axios'
 
 class Individual extends React.Component{
     constructor(){
         super()
         this.expand_signup=this.expand_signup.bind(this);
+        this.login=this.login.bind(this);
+        this.signup=this.signup.bind(this);
     }
 
     expand_signup(){
         document.getElementById("login-col").classList.add('shrink')
         document.getElementById("signup-col").classList.add('expand')
+    }
+
+    login(login_data){
+        console.log(login_data);
+        axios({
+            method:'post',
+            url:'http://localhost:8000/login',
+            data:login_data,
+            responseType:'json'
+        })
+        .then(response=>{
+            console.log(response);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+    }
+
+    signup(signup_data){
+        console.log(signup_data);
+        axios.post('https://a0d1abc26b8c.ngrok.io/signup',signup_data)
+        .then(response=>{
+            console.log(response);
+        })
+        .catch(error=>{
+            console.log(error);
+        })
     }
 
     render(){
@@ -20,11 +50,11 @@ class Individual extends React.Component{
                 <Row className="height_100">
                     <Col className="bg-white" id="login-col">
                         <Container fluid className="height_100">
-                            <LoginIndividual/>
+                            <LoginIndividual login={this.login}/>
                         </Container> 
                     </Col>
                     <Col className="d-flex flex-column justify-content-center bg-danger" id="signup-col" >
-                        <SignUpIndividual expand={this.expand_signup}/>
+                        <SignUpIndividual expand={this.expand_signup} signup={this.signup}/>
                     </Col>
                 </Row>
             </Container>
