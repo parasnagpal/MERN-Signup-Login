2ad572301cbe68ac01240e3c70d452afca2e9952
Paras Nagpal <paras.nag@gmail.com>
2020-06-06 04:33:48 +0530

    Capturing signup input

--- front end/src/components/individual/signup/signup.js
+++ front end/src/components/individual/signup/signup.js
@@ -1,19 +1,63 @@
 import React from 'react'
-import {Container,Row,Col,Button,Form,FormGroup,Input} from 'reactstrap'
+import {Container,Row,Col,Button,Form,FormGroup,Input,Label} from 'reactstrap'
 
 class SignUpIndividual extends React.Component{
     constructor(props){
         super(props);
         this.state={
             expanded:false,
+            email:"",
+            password:"",
+            password_confirm:"",
+            individual:false,
+            organization:false
         }
         this.signup=this.signup.bind(this);
+        this.emailchange=this.emailchange.bind(this);
+        this.passwordchange=this.passwordchange.bind(this);
+        this.passwordconfirmchange=this.passwordconfirmchange.bind(this);
+        this.organization=this.organization.bind(this);
+        this.individual=this.individual.bind(this);
     }
 
     signup(){
         this.props.expand();
         this.setState({
             expanded:true
+        })
+    }
+
+    emailchange(event){
+        this.setState({
+            email:event.target.value
+        })
+    }
+
+    passwordchange(event){
+        this.setState({
+            password:event.target.value
+        })
+    }
+
+    passwordconfirmchange(event){
+        this.setState({
+            password_confirm:event.target.value
+        },()=>{
+            console.log(event.target)
+        })
+    }
+
+    organization(){
+        this.setState({
+            organization:true,
+            individual:false
+        })
+    }
+
+    individual(){
+        this.setState({
+            organization:false,
+            individual:true
         })
     }
 
@@ -24,16 +68,30 @@ class SignUpIndividual extends React.Component{
                 <Row className="d-flex justify-content-center">
                     <Form row>
                         <FormGroup>
-                            <Input placeholder="Email"></Input>
+                            <Input placeholder="Email" type='email' value={this.state.email} onChange={this.emailchange}></Input>
                         </FormGroup>
                         <FormGroup>
-                            <Input placeholder="Password"></Input>
+                            <Input placeholder="Password" type='password' value={this.state.password} onChange={this.passwordchange}></Input>
                         </FormGroup>
                         <FormGroup>
-                        <Input placeholder="Confirm Password"></Input>
+                            <Input placeholder="Confirm Password" type='password' value={this.state.password_confirm} onChange={this.passwordconfirmchange}></Input>
                         </FormGroup>
+                        <div className="d-flex">
+                        <FormGroup check className="d-flex justify-content-start m-2">
+                            <Label check>
+                              <Input type="radio" name="radio2" onChange={this.individual}/>{' '}
+                              Individual
+                            </Label>
+                        </FormGroup>
+                        <FormGroup check className="d-flex justify-content-start m-2">
+                            <Label check>
+                              <Input type="radio" name="radio2" onChange={this.organization}/>{' '}
+                              Organization
+                            </Label>
+                        </FormGroup>
+                        </div>
                         <FormGroup>
-                        <Button color='light' outline >Sign Up</Button>
+                            <Button color='light' outline onClick={()=>{this.props.signup(this.state)}}>Sign Up</Button>
                         </FormGroup>
                     </Form>
                 </Row>
