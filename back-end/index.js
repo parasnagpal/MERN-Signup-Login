c1eeb92449c5c802495671438ceed358fb2e93cf
Paras Nagpal <paras.nag@gmail.com>
2020-06-06 20:47:28 +0530

    Handling loging signup credentials

--- back-end/index.js
+++ back-end/index.js
@@ -5,11 +5,12 @@ const cors=require('cors');
 
 const url='mongodb://localhost:27017';
 const dbName='mern';
+let db
 
 MongoClient.connect(url,(err,client)=>{
     console.log("Connected successfully to server");
-    const db=client.db(dbName);
-    client.close();
+    db=client.db(dbName);
+    //client.close();
 })
 
 app.use(express.json());
@@ -23,13 +24,49 @@ app.get('/',function(req,res){
     res.send('Hello world');
 })
 
-app.post('/login',function(req,res){
-    console.log(req.body);
-    res.json('received login details');
+app.post('/login',async function(req,res){
+
+    if(req.body.individual)
+    await db.collection('individual').find({
+        email:req.body.email,
+        password:req.body.password
+    })
+    .toArray((err,data)=>{
+        //console.log(data.length)
+        if(data.length===0)
+            res.status(400).json("Invalid login credentials");
+        else    
+            res.status(200).json("Individual Login Success");
+    })
+    
+    else
+    await db.collection('organisation').find({
+        email:req.body.email,
+        password:req.body.password
+    })
+    .toArray((err,data)=>{
+        //console.log(data.length)
+        if(data.length===0)
+            res.status(400).json("Invalid login credentials");
+        else    
+            res.status(200).json("Organisation Login Success");
+    })
+
 })
 
-app.post('/signup',function(req,res){
+app.post('/signup',async function(req,res){
     console.log(req.body);
+    let collection_name=req.body.individual?'individual':'organisation'    
+    await db.collection(collection_name).insertOne({
+        email:req.body.email,
+        password:req.body.password
+    })
+    .then(data=>{
+        console.log('insert successful');
+    })
+    .catch(err=>{
+        console.log(err);
+    })
     res.json('received signup details');
 })
 
